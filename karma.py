770d977cd07211f0c7363093ae53570e4f156008
Daniel Dao <daniel.vu.dao@gmail.com>
2014-02-17 00:25:20 -0600

    Grabs top karma of user by subreddit

---
+++ karma.py
@@ -0,0 +1,16 @@
+import praw
+
+r = praw.Reddit(user_agent='Karma breakdown 1.0 by /u/danielvd')
+print "Enter the username"
+user_name = str(raw_input())
+user = r.get_redditor(user_name)
+thing_limit = 10 
+gen = user.get_submitted(limit=thing_limit)
+karma_by_subreddit = {}
+
+for thing in gen: 
+	subreddit = thing.subreddit.display_name
+	karma_by_subreddit[subreddit] = (karma_by_subreddit.get(subreddit,0) + thing.score)
+	print subreddit, thing.score
+
+
