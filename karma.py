42e883356a9b8708c9f3fdbe0b52c003d318140b
Daniel Dao <daniel.vu.dao@gmail.com>
2014-02-18 17:42:32 -0600

    Fixed trailing white spaces problem

--- karma.py
+++ karma.py
@@ -3,6 +3,7 @@ import praw
 r = praw.Reddit(user_agent='Karma breakdown 1.0 by /u/danielvd')
 print "Enter the username"
 user_name = str(raw_input())
+user_name = user_name.rstrip()
 user = r.get_redditor(user_name)
 thing_limit = 10 
 gen = user.get_submitted(limit=thing_limit)
